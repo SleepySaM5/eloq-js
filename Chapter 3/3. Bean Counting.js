22233a31d2b4638b34a3e46a21f3936e31d4e884
vtibi <vtibii@gmail.com>
2016-10-07 14:27:37 +0200

    Added exercise 3

--- Chapter 3/3. Bean Counting.js
+++ Chapter 3/3. Bean Counting.js
@@ -3,5 +3,23 @@
  */
 
 function countBs(string) {
-	
+	var count = 0;
+	for(var i = 0; i < string.length; i++) {
+		if(string.charAt(i) == "B") 
+			count++;
+	}
+	return count;
 }
+
+console.log(countBs("BBC"));
+
+function countChar(string, char) {
+	var count = 0;
+	for(var i = 0; i < string.length; i++) {
+		if(string.charAt(i) == char)
+			count++;
+	}
+	return count;
+}
+
+console.log(countChar("BBBBBBC", "B"));
